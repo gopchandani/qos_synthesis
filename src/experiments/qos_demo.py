a7145e7fb68bb271a69180d0bd2bc95a4ac3e3fe
Rakesh Kumar <gopchandani@gmail.com>
2016-10-01 14:02:10 -0700

    Got me a first half-decent plot here

--- src/experiments/qos_demo.py
+++ src/experiments/qos_demo.py
@@ -1,5 +1,6 @@
 __author__ = 'Rakesh Kumar'
 
+import matplotlib.pyplot as plt
 import sys
 import time
 from collections import defaultdict
@@ -20,7 +21,7 @@ class QosDemo(Experiment):
                  network_configurations,
                  num_measurements):
 
-        super(QosDemo, self).__init__("number_of_hosts", num_iterations)
+        super(QosDemo, self).__init__("qos_demo", num_iterations)
         self.network_configurations = network_configurations
         self.num_measurements = num_measurements
 
@@ -29,11 +30,126 @@ class QosDemo(Experiment):
         time.sleep(5)
         self.controller_port = self.cm.start_controller()
 
+    def prepare_data(self):
+
         self.data = {
             "Throughput": defaultdict(defaultdict),
+            "Mean Latency": defaultdict(defaultdict),
             "99th Percentile Latency": defaultdict(defaultdict),
             "Maximum Latency": defaultdict(defaultdict)
         }
+
+        for nc in self.network_configurations:
+
+            for fs in nc.flow_specs:
+
+                if not fs.measurement_rates:
+                    continue
+
+                first_key = fs.src_host_id + "->" + fs.dst_host_id +\
+                            " same_output_queue = " + str(nc.synthesis_params["same_output_queue"])
+
+                second_key = nc.topo_params["num_hosts_per_switch"]
+
+                self.data["Throughput"][first_key][second_key] = []
+                self.data["Mean Latency"][first_key][second_key] = []
+                self.data["99th Percentile Latency"][first_key][second_key] = []
+                self.data["Maximum Latency"][first_key][second_key] = []
+
+                for measurement in fs.measurements[fs.configured_rate]:
+                    self.data["Throughput"][first_key][second_key].append(float(measurement["throughput"]))
+                    self.data["Mean Latency"][first_key][second_key].append(float(measurement["mean_latency"]))
+                    self.data["99th Percentile Latency"][first_key][second_key].append(float(measurement["nn_perc_latency"]))
+                    self.data["Maximum Latency"][first_key][second_key].append(float(measurement["max_latency"]))
+
+        print "here"
+
+    def plot_qos(self):
+
+        f, (ax1, ax2, ax3) = plt.subplots(1, 3, sharex=False, sharey=False, figsize=(9.5, 3.0))
+
+        data_xticks = [2]#self.network_configurations[0].topo_params["num_hosts_per_switch"]
+        data_xtick_labels = ["2"]# [str(x) for x in data_xticks]
+
+        self.plot_lines_with_error_bars(ax1,
+                                        "Throughput",
+                                        "",
+                                        "Throughput(Mbps)",
+                                        "(a)",
+                                        y_scale='linear',
+                                        x_min_factor=1.0,
+                                        x_max_factor=1.0,
+                                        y_min_factor=0.01,
+                                        y_max_factor=1,
+                                        xticks=data_xticks,
+                                        xtick_labels=data_xtick_labels)
+
+        ax1.set_ylim(46.5, 47.5)
+
+        self.plot_lines_with_error_bars(ax2,
+                                        "Mean Latency",
+                                        "",
+                                        "Mean Latency(ms)",
+                                        "(b)",
+                                        y_scale='linear',
+                                        x_min_factor=1.0,
+                                        x_max_factor=1.0,
+                                        y_min_factor=0.1,
+                                        y_max_factor=1.2,
+                                        xticks=data_xticks,
+                                        xtick_labels=data_xtick_labels)
+
+        self.plot_lines_with_error_bars(ax3,
+                                        "99th Percentile Latency",
+                                        "Number of host pairs per switch",
+                                        "99th Percentile Latency(ms)",
+                                        "(c)",
+                                        y_scale='linear',
+                                        x_min_factor=1.0,
+                                        x_max_factor=1.0,
+                                        y_min_factor=0.01,
+                                        y_max_factor=1,
+                                        xticks=data_xticks,
+                                        xtick_labels=data_xtick_labels)
+
+
+        xlabels = ax1.get_xticklabels()
+        plt.setp(xlabels, rotation=0, fontsize=8)
+
+        xlabels = ax2.get_xticklabels()
+        plt.setp(xlabels, rotation=0, fontsize=8)
+
+        xlabels = ax3.get_xticklabels()
+        plt.setp(xlabels, rotation=0, fontsize=8)
+
+        # Shrink current axis's height by 25% on the bottom
+        box = ax1.get_position()
+        ax1.set_position([box.x0, box.y0 + box.height * 0.3,
+                          box.width, box.height * 0.7])
+
+        box = ax2.get_position()
+        ax2.set_position([box.x0, box.y0 + box.height * 0.3,
+                          box.width, box.height * 0.7])
+
+        box = ax3.get_position()
+        ax3.set_position([box.x0, box.y0 + box.height * 0.3,
+                          box.width, box.height * 0.7])
+
+        handles, labels = ax3.get_legend_handles_labels()
+
+        ax1.legend(handles,
+                   labels,
+                   shadow=True,
+                   fontsize=8,
+                   loc='upper center',
+                   ncol=2,
+                   markerscale=1.0,
+                   frameon=True,
+                   fancybox=True,
+                   columnspacing=0.5, bbox_to_anchor=[1.6, -0.27])
+
+        plt.savefig("plots/" + self.experiment_tag + "_" + "qos_demo" + ".png", dpi=100)
+        plt.show()
 
     def trigger(self):
 
@@ -45,10 +161,8 @@ class QosDemo(Experiment):
             nc.synthesis.synthesize_flow_specifications(nc.flow_specs)
             self.measure_flow_rates(nc)
 
-        print "here"
-
     def parse_iperf_output(self, iperf_output_string):
-        data_lines =  iperf_output_string.split('\r\n')
+        data_lines = iperf_output_string.split('\r\n')
         interesting_line_index = None
         for i in xrange(len(data_lines)):
              if data_lines[i].endswith('Server Report:'):
@@ -162,7 +276,14 @@ def main():
 
     exp = QosDemo(num_iterations, network_configurations, len(measurement_rates))
 
-    exp.trigger()
+    # exp.trigger()
+    # exp.prepare_data()
+    # exp.dump_data()
+
+    #exp.load_data("data/qos_demo_2_iterations_20161001_125409.json")
+    exp.load_data("data/qos_demo_10_iterations_20161001_125929.json")
+
+    exp.plot_qos()
 
 if __name__ == "__main__":
     main()
