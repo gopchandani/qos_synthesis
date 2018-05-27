c39ef3231c888b0aae667c6827f7b5ddf9ae9b29
Rakesh Kumar <kumar19@illinois.edu>
2018-05-26 20:56:57 -0500

    More values for processing time.

--- SwitchProcessingTime/processing_time_esrtimate.py
+++ SwitchProcessingTime/processing_time_esrtimate.py
@@ -1,5 +1,4 @@
 
-
 from data_summary import get_data_dict, periods_str, num_switches_strs, payloads_strs
 data_dict = get_data_dict()
 
@@ -9,21 +8,34 @@ data_dict = get_data_dict()
 # Take the mean end-to-end delay for four switches (fs) and single switch(ss)
 # Then, amortized switch processing time = (fs - ss) / 3
 
-fs = data_dict['100ms']['4']['256B']["mean"]
-ss = data_dict['100ms']['1']['256B']["mean"]
+# fs = data_dict['100ms']['4']['256B']["mean"]
+# ss = data_dict['100ms']['1']['256B']["mean"]
+#
+# print("Time with Laptops @ 100ms, 256B:", (fs - ss)/3)
+#
+# fs = data_dict['1000ms']['4']['256B']["mean"]
+# ss = data_dict['1000ms']['1']['256B']["mean"]
+#
+# print("Time with Pis @ 1000ms, 256B:", (fs - ss)/3)
 
-print "Time with Laptops @ 100ms, 256B:", (fs - ss)/3
 
-fs = data_dict['1000ms']['4']['256B']["mean"]
-ss = data_dict['1000ms']['1']['256B']["mean"]
+# fs = data_dict['100ms']['4']['1024B']["mean"]
+# ss = data_dict['100ms']['1']['1024B']["mean"]
+#
+# print("Time with Laptops @ 100ms, 1024B:", (fs - ss)/3)
 
-print "Time with Pis @ 1000ms, 256B:", (fs - ss)/3
+fs = data_dict['1000ms']['4']['1024B']["mean"]
+ss = data_dict['1000ms']['1']['1024B']["mean"]
 
-fs = data_dict['100ms']['4']['1408B']["mean"]
-ss = data_dict['100ms']['1']['1408B']["mean"]
-print "Time with Laptops @ 100ms, 1408B:", (fs - ss)/3
+print("Time with Pis @ 1000ms, 1024B:", (fs - ss)/3)
 
-fs = data_dict['1000ms']['4']['1408B']["mean"]
-ss = data_dict['1000ms']['1']['1408B']["mean"]
-print "Time with Pis @ 1000ms, 1408B:", (fs - ss)/3
-
+#
+#
+# fs = data_dict['100ms']['4']['1408B']["mean"]
+# ss = data_dict['100ms']['1']['1408B']["mean"]
+# print("Time with Laptops @ 100ms, 1408B:", (fs - ss)/3)
+#
+# fs = data_dict['1000ms']['4']['1408B']["mean"]
+# ss = data_dict['1000ms']['1']['1408B']["mean"]
+# print("Time with Pis @ 1000ms, 1408B:", (fs - ss)/3)
+#
