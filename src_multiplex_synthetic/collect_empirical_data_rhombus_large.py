813e1df59133aec4ef87d47cc443664152be4684
Ashish Kashinath <akashina@eng.ucsd.edu>
2018-08-14 12:39:21 -0500

    Changes

--- src_multiplex_synthetic/collect_empirical_data_rhombus_large.py
+++ src_multiplex_synthetic/collect_empirical_data_rhombus_large.py
@@ -1,59 +1,28 @@
 import paramiko
 import os
+import numpy as np
 import threading
+import csv
 import datetime
+from collections import defaultdict
+from flow_dict import all_flows as flows
+#from flow_dict import flow_f1 as flows
+#from flow_dict import flow_f2 as flows
+#from flow_dict import flow_f3 as flows
+#from flow_dict import flow_f4 as flows
+#from flow_dict import flow_f5 as flows
+#from flow_dict import flow_f6 as flows
+#from flow_dict import flow_f7 as flows
+#from flow_dict import flow_f8 as flows
+from topo_dict import host_ip
 
-host_ip = {
-    "dot08": ["192.168.1.8", "10.192.180.112"],
-    "dot09": ["192.168.1.9", "10.193.188.190"],
-    "dot10": ["192.168.1.10", "10.193.198.88"],
-    "dot11": ["192.168.1.11", "10.192.92.86"],
-    "dot12": ["192.168.1.12", "10.193.57.162"],
-    "dot15": ["192.168.1.15", "10.195.168.155"],
-    "dot20": ["192.168.1.20", "10.194.58.113"],
-    "dot29": ["192.168.1.29", "10.192.189.214"],
-    "dot30": ["192.168.1.30", "10.195.27.95"],
-    "dot31": ["192.168.1.31", "10.193.242.20"],
-    "dot40": ["192.168.1.40", "10.193.31.152"]
-}
 
 client_process = "`echo $HOME`/Repositories/qos_synthesis/traffic_generation/udp_client/client"
 server_process = "`echo $HOME`/Repositories/qos_synthesis/traffic_generation/udp_server/server"
-
-
-should_nice = False
-
-flows = [
-    {"id": "f1", "port": 10000, "client": "dot08", "server": "dot40", "rate": 20,
-     "data_loc": "/home/pi/", "user": "pi"},
-    {"id": "f2", "port": 10001, "client": "dot10", "server": "dot40", "rate": 20,
-     "data_loc": "/home/pi/", "user": "pi"},
-    {"id": "f3", "port": 10002, "client": "dot12", "server": "dot40", "rate": 20,
-     "data_loc": "/home/pi/", "user": "pi"},
-    {"id": "f4", "port": 10003, "client": "dot09", "server": "dot20", "rate": 20,
-     "data_loc": "/home/iti/", "user": "iti"},
-    {"id": "f5", "port": 10004, "client": "dot10", "server": "dot20", "rate": 20,
-     "data_loc": "/home/iti/", "user": "iti"},
-    {"id": "f6", "port": 10005, "client": "dot11", "server": "dot20", "rate": 20,
-     "data_loc": "/home/iti/", "user": "iti"},
-    {"id": "f7", "port": 10006, "client": "dot29", "server": "dot20", "rate": 20,
-     "data_loc": "/home/iti/", "user": "iti"},
-    {"id": "f8", "port": 10007, "client": "dot30", "server": "dot20", "rate": 20,
-     "data_loc": "/home/iti/", "user": "iti"},
-    {"id": "f9", "port": 10008, "client": "dot31", "server": "dot20", "rate": 20,
-     "data_loc": "/home/iti/", "user": "iti"},
-    {"id": "f10", "port": 10009, "client": "dot15", "server": "dot40", "rate": 20,
-     "data_loc": "/home/pi/", "user": "pi"},
-    {"id": "f11", "port": 10010, "client": "dot15", "server": "dot40", "rate": 20,
-     "data_loc": "/home/pi/", "user": "pi"}
-]
+should_nice = True
 
 
 def run_cmd_via_paramiko(IP, command, port=22, username='pi', password='raspberry'):
-
-    if IP == host_ip['dot20'][1]:
-        username = 'iti'
-        password = 'csl440'
 
     s = paramiko.SSHClient()
     s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -93,7 +62,6 @@ def client_command(server_ip, num_packets, flow_rate_in_Mbps, port_no):
 def server_command(port_no, num_packets, flow_num, is_background, is_same):
     file_name = '_'.join([flow_num, is_background, is_same])
     output_file = '.'.join([file_name, 'csv'])
-    #server_command = ' '.join([server_process, port_no, num_packets]) + ' > ' + output_file
     server_command = ' '.join([server_process, port_no, num_packets]) + ' ' + output_file
     if should_nice == True:
         server_command = 'sudo nice --20 ' + server_command
@@ -101,6 +69,31 @@ def server_command(port_no, num_packets, flow_num, is_background, is_same):
     print server_command
     return server_command
 
+
+def update_traffic_repo():
+    for client in host_ip:
+        print run_cmd_via_paramiko(host_ip[client][1], "cd Repositories/qos_synthesis; cd traffic_generation/;"
+                                                       "cd udp_client/; make clean"
+                                                       "cd ../udp_server/; make clean"
+                                                       "cd ../../"
+                                                       "git pull;")
+
+
+def compile_traffic_repo():
+    for client in host_ip:
+        print run_cmd_via_paramiko(host_ip[client][1], "cd Repositories/qos_synthesis/traffic_generation/; "
+                                                       "cd udp_server/; make; cd ../udp_client/; make;")
+
+
+def clean_client_server():
+    for client in host_ip:
+        print run_cmd_via_paramiko(host_ip[client][1], "sudo pkill -x client; sudo pkill -x server; rm *.csv")
+
+def update_pi_scripts():
+    for client in host_ip:
+        print run_cmd_via_paramiko(host_ip[client][1], "cd scripts/; rm -rf *; git init; git remote add origin "
+                                                       "https://github.com/gopchandani/qos_synthesis.git; git pull;"
+                                                       "git checkout pi_scripts ")
 
 def start_all_flows_simultaneously(num_packets, is_background, is_same):
     server_thread_list = []
@@ -133,27 +126,31 @@ def start_all_flows_simultaneously(num_packets, is_background, is_same):
     print 'Clients have finished sending the packets'
 
     for server_thread in server_thread_list:
-        server_thread.join(5.0)
+        server_thread.join(10.0)
 
     print 'Servers have finished'
 
 def main():
 
+    # update_pi_scripts()
+    clean_client_server()
+    # update_traffic_repo()
+    # compile_traffic_repo()
+
     num_packets = input('Enter the number of packets')
     background_traffic = input('Is there a background flow?')
     paths = input('Is it same or different paths?')
 
-    for client in host_ip:
-        print run_cmd_via_paramiko(host_ip[client][1], "sudo pkill -x client; sudo pkill -x server")
-        #print run_cmd_via_paramiko(host_ip[client][1], "cd Repositories/qos_synthesis/traffic_generation/udp_server; make clean; make;")
-        #print run_cmd_via_paramiko(host_ip[client][1], "cd Repositories/qos_synthesis/traffic_generation/udp_client; make clean; make;")
-
     is_background = 'bg' if background_traffic else 'nobg'
     is_same = 'same_paths' if paths else 'diff_paths'
     start_all_flows_simultaneously(num_packets, is_background, is_same)
-
     get_rhombus_data_pscp('_'.join([is_background, is_same]))
-    return
+
+    #output_dir = get_rhombus_data_pscp('_'.join([is_background, is_same]))
+    #processed_expt_output(os.getcwd() + "/" + output_dir)
+    #processed_expt_output(os.getcwd() + "/" + "allflows_nobg")
+
+    return 0
 
 
 def get_rhombus_data(file_suffix):
@@ -163,6 +160,7 @@ def get_rhombus_data(file_suffix):
         cmd = "scp " + f["user"] + "@" + host_ip[f["server"]][1] + ":" + remote_filepath + " " + local_data_location
         os.system(cmd)
 
+
 def get_rhombus_data_pscp(file_suffix):
 
     mydir = os.path.join(os.getcwd(), datetime.datetime.now().strftime('%Y%m%d_%H%M%S'))
@@ -170,13 +168,39 @@ def get_rhombus_data_pscp(file_suffix):
 
     for f in flows:
         remote_filepath = f["data_loc"] + f["id"] + '_' + file_suffix + ".csv"
-        if host_ip[f["server"]][0] == host_ip["dot40"][0]:
-            cmd = "pscp " + "-pw raspberry " + f["user"] + "@" + host_ip[f["server"]][1] + ":" + remote_filepath + " " + mydir + '/'
-        else:
-            cmd = "pscp " + "-pw csl440 " + f["user"] + "@" + host_ip[f["server"]][1] + ":" + remote_filepath + " " + mydir + '/'
-        print cmd
 
+        cmd = "pscp " + "-pw raspberry " + f["user"] + "@" + host_ip[f["server"]][1] + ":" + remote_filepath + " " + mydir + '/'
+        print cmd
         os.system(cmd)
+
+    return mydir
+
+
+def processed_expt_output(directory):
+
+    output = defaultdict(list)
+
+    for f in os.listdir(directory):
+        with open(directory + '/' + f) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                if row[" One-way delay(ns)"] is not None:
+                    try:
+                        output[f].append(int(row[" One-way delay(ns)"]))
+                    except ValueError:
+                        pass
+
+    for flow in output:
+        print("Flow: %s, #-Samples: %s, Avg: %s, Stdev: %s, 50th Percentile: %s, 99th Percentile: %s, 99.9th Percentile: %s" % (
+            flow,
+            len(output[flow]),
+            np.mean(output[flow]),
+            np.std(output[flow]),
+            np.percentile(output[flow], 50),
+            np.percentile(output[flow], 99),
+            np.percentile(output[flow], 99.9)
+        ))
+
 
 if __name__ == "__main__":
     main()
---
+++ src_multiplex_synthetic/flow_dict.py
@@ -0,0 +1,98 @@
+
+all_flows = [
+    {"id": "f1", "port": 10000, "client": "dot08", "server": "dot09", "rate": 2,
+     "data_loc": "/home/pi/", "user": "pi"},
+    {"id": "f2", "port": 10001, "client": "dot10", "server": "dot11", "rate": 2,
+     "data_loc": "/home/pi/", "user": "pi"},
+    {"id": "f3", "port": 10002, "client": "dot12", "server": "dot15", "rate": 2,
+     "data_loc": "/home/pi/", "user": "pi"},
+    {"id": "f4", "port": 10003, "client": "dot20", "server": "dot29", "rate": 2,
+     "data_loc": "/home/pi/", "user": "pi"},
+    {"id": "f5", "port": 10004, "client": "dot30", "server": "dot31", "rate": 2,
+     "data_loc": "/home/pi/", "user": "pi"},
+    {"id": "f6", "port": 10005, "client": "dot40", "server": "dot120", "rate": 2,
+     "data_loc": "/home/pi/", "user": "pi"},
+    {"id": "f7", "port": 10006, "client": "dot140", "server": "dot200", "rate": 2,
+     "data_loc": "/home/pi/", "user": "pi"},
+    {"id": "f8", "port": 10007, "client": "dot220", "server": "dot240", "rate": 2,
+     "data_loc": "/home/pi/", "user": "pi"}
+]
+
+# all_flows = [
+#     {"id": "f1", "port": 10000, "client": "dot08", "server": "dot09", "rate": 30,
+#      "data_loc": "/home/pi/", "user": "pi"},
+#     {"id": "f2", "port": 10001, "client": "dot10", "server": "dot11", "rate": 30,
+#      "data_loc": "/home/pi/", "user": "pi"},
+#     {"id": "f3", "port": 10002, "client": "dot12", "server": "dot15", "rate": 30,
+#      "data_loc": "/home/pi/", "user": "pi"},
+#     {"id": "f4", "port": 10003, "client": "dot20", "server": "dot29", "rate": 30,
+#      "data_loc": "/home/pi/", "user": "pi"},
+#     {"id": "f5", "port": 10004, "client": "dot30", "server": "dot31", "rate": 30,
+#      "data_loc": "/home/pi/", "user": "pi"},
+#     {"id": "f6", "port": 10005, "client": "dot40", "server": "dot120", "rate": 30,
+#      "data_loc": "/home/pi/", "user": "pi"},
+#     {"id": "f7", "port": 10006, "client": "dot140", "server": "dot200", "rate": 30,
+#      "data_loc": "/home/pi/", "user": "pi"},
+#     {"id": "f8", "port": 10007, "client": "dot220", "server": "dot240", "rate": 30,
+#      "data_loc": "/home/pi/", "user": "pi"}
+# ]
+
+# all_flows = [
+#     {"id": "f1", "port": 10000, "client": "dot08", "server": "dot09", "rate": 10,
+#      "data_loc": "/home/pi/", "user": "pi"},
+#     {"id": "f2", "port": 10001, "client": "dot10", "server": "dot11", "rate": 10,
+#      "data_loc": "/home/pi/", "user": "pi"},
+#     {"id": "f3", "port": 10002, "client": "dot12", "server": "dot15", "rate": 10,
+#      "data_loc": "/home/pi/", "user": "pi"},
+#     {"id": "f4", "port": 10003, "client": "dot20", "server": "dot29", "rate": 10,
+#      "data_loc": "/home/pi/", "user": "pi"},
+#     {"id": "f5", "port": 10004, "client": "dot30", "server": "dot31", "rate": 10,
+#      "data_loc": "/home/pi/", "user": "pi"},
+#     {"id": "f6", "port": 10005, "client": "dot40", "server": "dot120", "rate": 10,
+#      "data_loc": "/home/pi/", "user": "pi"},
+#     {"id": "f7", "port": 10006, "client": "dot140", "server": "dot200", "rate": 10,
+#      "data_loc": "/home/pi/", "user": "pi"},
+#     {"id": "f8", "port": 10007, "client": "dot220", "server": "dot240", "rate": 10,
+#      "data_loc": "/home/pi/", "user": "pi"}
+# ]
+
+flow_f1 = [
+    {"id": "f1", "port": 10000, "client": "dot08", "server": "dot09", "rate": 10,
+     "data_loc": "/home/pi/", "user": "pi"}
+]
+
+flow_f2 = [
+    {"id": "f2", "port": 10001, "client": "dot10", "server": "dot11", "rate": 10,
+     "data_loc": "/home/pi/", "user": "pi"}
+]
+
+flow_f3 = [
+    {"id": "f3", "port": 10002, "client": "dot12", "server": "dot15", "rate": 10,
+     "data_loc": "/home/pi/", "user": "pi"}
+]
+
+flow_f4 = [
+    {"id": "f4", "port": 10003, "client": "dot20", "server": "dot29", "rate": 10,
+     "data_loc": "/home/pi/", "user": "pi"}
+]
+
+
+flow_f5 = [
+    {"id": "f5", "port": 10004, "client": "dot30", "server": "dot31", "rate": 10,
+     "data_loc": "/home/pi/", "user": "pi"}
+]
+
+flow_f6 = [
+    {"id": "f6", "port": 10005, "client": "dot40", "server": "dot120", "rate": 10,
+     "data_loc": "/home/pi/", "user": "pi"}
+]
+
+flow_f7 = [
+    {"id": "f7", "port": 10006, "client": "dot140", "server": "dot200", "rate": 10,
+     "data_loc": "/home/pi/", "user": "pi"}
+]
+
+flow_f8 = [
+    {"id": "f8", "port": 10007, "client": "dot220", "server": "dot240", "rate": 10,
+     "data_loc": "/home/pi/", "user": "pi"}
+]
